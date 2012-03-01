5453a4c777028e999d457437eb119f250d52f612
Mike Plavsky <Mike.Plavsky@quest.com>
2012-03-01 17:36:42 +0000

    dirs generator

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 #!/usr/bin/python
 
 from jinja2 import Environment, FileSystemLoader
+import os
 
-path2templates = ["./tests/queries/templates/queries", "./tests/queries/templates/common"]
-path2result = "./tests/queries"
+templates_dirs = ["./tests/queries/templates/queries", "./tests/queries/templates/common"]
+path2templates = './tests/queries/templates/queries'
+path2result = './tests/queries'
 
-env = Environment(loader = FileSystemLoader( path2templates ))
+env = Environment(loader = FileSystemLoader( templates_dirs ))
 
-def render(name, fn):
+def render(tmpl):
 
-  template = env.get_template( name )
+  template = env.get_template( tmpl[1] )
   res = template.render()
 
-  import os
-  open(os.path.join(path2result, fn), 'w' ).write( res )
+  open(tmpl[0], 'w' ).write( res )
 
-render( "report1/bad_example1.sql", "bad_example1.sql" )
-render( "report2/bad_example2.sql", "bad_example2.sql" )
-render( "report3/good_example1.sql", "good_example1.sql" )
+def get_templates(tmpl_path):
+
+  for path, _, filelist in os.walk(tmpl_path):
+    for name in filelist:
+
+      report = os.path.split(path)[1]
+      yield [report, os.path.join(report,name), name]
 
 
+def mkdir(where, templates):
+
+  for tmpl in templates:
+
+    import shutil
+    path = os.path.join(where, tmpl[0])
+
+    shutil.rmtree(path, ignore_errors=True)
+    os.makedirs( path )
+
+    tmpl[0] = os.path.join( path, tmpl[2] )
+
+    yield tmpl
+
+templates =  get_templates(path2templates)
+for x in mkdir(path2result, templates): render(x)
+    
+    
